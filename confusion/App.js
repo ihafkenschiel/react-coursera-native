ea1b6b7f998f96f3c9387303d25f3e56db2cc3e4
Ian <ihafkenschiel@gmail.com>
2020-01-31 01:12:25 -0300

    Changed to same format as tutorial

--- confusion/App.js
+++ confusion/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is Ian editing the app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is Ian editing the app. Now using Class.</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
