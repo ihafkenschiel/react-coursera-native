f23c44372a7abda915f9ccf7788e4e6ea9aa35f2
Ian <ihafkenschiel@gmail.com>
2020-03-07 20:00:09 -0300

    Added server file for Typicode online https server

--- server/https-json-server.js
+++ server/https-json-server.js
@@ -7,14 +7,25 @@ const jsonServer = require("json-server");
 const https = require("https");
 const path = require("path");
 const fs = require("fs");
-const pause = require('connect-pause');
+const pause = require('connect-pause'); // Install: npm install connect-pause
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
 server.use(pause(2000));
 server.use(router);
 
