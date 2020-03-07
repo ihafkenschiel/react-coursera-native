5cdd9886704ef73f43800989cd6898b0c6d5463b
Ian <ihafkenschiel@gmail.com>
2020-03-07 20:13:47 -0300

    Fixed baseUrl which was messing up server access

--- confusion/shared/baseUrl.js
+++ confusion/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseURL = 'https://192.168.0.100:3001/'
+//export const baseUrl = 'https://my-json-server.typicode.com/ihafkenschiel/react-coursera-native/'
+export const baseUrl = 'http://192.168.0.100:3001/'
