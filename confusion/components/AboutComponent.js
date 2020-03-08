8340f127a5fccdf354a776144b3283674b9253a3
Ian <ihafkenschiel@gmail.com>
2020-03-07 22:50:46 -0300

    Updated AboutComponent to remove CorporateLeaders class the way the teacher has it

--- confusion/components/AboutComponent.js
+++ confusion/components/AboutComponent.js
@@ -13,34 +13,8 @@ const mapStateToProps = state => {
 function History() {
   return(
     <Card title="Our History">
-      <Text>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
-      <Text></Text>
-      <Text>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
-    </Card>
-  );
-}
-
-function CorporateLeadership(props) {
-
-  const renderLeaders = ({item, index}) => {
-    return(
-      <ListItem
-          key={index}
-          title={item.name}
-          subtitle={item.description}
-          hideChevron={true}
-          leftAvatar={{source: {uri: baseUrl + item.image}}}
-        />
-    );
-  }
-
-  return(
-    <Card title="Corporate Leadership">
-      <FlatList
-        data={props.leaders}
-        renderItem={renderLeaders}
-        keyExtractor={item => item.id.toString()}
-      />
+      <Text style={{margin: 10}}>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
+      <Text style={{margin: 10}}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
     </Card>
   );
 }
@@ -53,10 +27,31 @@ class About extends Component {
   }
 
   render() {
+    const { params } = this.props.navigation.state;
+    const renderLeader = ({item, index}) => {
+      return(
+        <ListItem
+            roundAvatar
+            key={index}
+            title={item.name}
+            subtitle={item.description}
+            subtitleNumberOfLines={15}
+            hideChevron={true}
+            leftAvatar={{source: {uri: baseUrl + item.image}}}
+          />
+      );
+    }
+
     return(
       <ScrollView>
         <History />
-        <CorporateLeadership leaders={this.props.leaders.leaders} />
+        <Card title="Corporate Leadership">
+          <FlatList
+            data={this.props.leaders.leaders}
+            renderItem={renderLeader}
+            keyExtractor={item => item.id.toString()}
+          />
+        </Card>
       </ScrollView>
     );
   }
