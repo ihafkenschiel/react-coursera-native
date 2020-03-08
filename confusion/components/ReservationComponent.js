fb4688491f601309c57d0ef0c04fe229136662d3
Ian <ihafkenschiel@gmail.com>
2020-03-08 20:19:49 -0300

    Modals

--- confusion/components/ReservationComponent.js
+++ confusion/components/ReservationComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 
@@ -10,7 +10,8 @@ class Reservation extends Component {
     this.state = {
       guests: 1,
       smoking: false,
-      date: ''
+      date: '',
+      showModal: false
     }
   }
 
@@ -18,10 +19,16 @@ class Reservation extends Component {
     title: 'Reserve Table'
   }
 
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
   handleReservation() {
     console.log(JSON.stringify(this.state));
+    this.toggleModal();
+  }
 
-    // Reset form
+  resetForm() {
     this.setState({
       guests: 1,
       smoking: false,
@@ -90,6 +97,25 @@ class Reservation extends Component {
             accessibilityLabel='Learn more about this purple button'
           />
         </View>
+        <Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={this.state.showModal}
+          onDismiss={() => {  this.resetForm() } }
+          onRequestClose={() => { this.toggleModal(); this.resetForm() } }
+        >
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>Your Reservation</Text>
+            <Text style={styles.modalText}>Number of Guests: {this.state.guests}</Text>
+            <Text style={styles.modalText}>Smoking?: {this.state.smoking ? 'Yes' : 'No'}</Text>
+            <Text style={styles.modalText}>Date and Time: {this.state.date}</Text>
+          <Button
+            onPress={() => { this.toggleModal(); this.resetForm() } }
+            color='#512DA8'
+            title='Close'
+          />
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
@@ -110,6 +136,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1
+  },
+  modal: {
+    justifyContent: 'center',
+    margin: 20
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#512DA8',
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 20
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10
   }
 });
 
--- confusion/shared/baseUrl.js
+++ confusion/shared/baseUrl.js
@@ -1,2 +1,2 @@
 //export const baseUrl = 'https://my-json-server.typicode.com/ihafkenschiel/react-coursera-native/'
-export const baseUrl = 'http://192.168.15.77:3001/'
+export const baseUrl = 'http://192.168.0.100:3001/'
