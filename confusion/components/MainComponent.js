133e699266fe99b7a0c3e323a08d962fc445d6c8
Ian <ihafkenschiel@gmail.com>
2020-03-08 17:40:45 -0300

    Created form component

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -103,6 +104,24 @@ const ContactNavigator = createStackNavigator({
     })
 });
 
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name='menu'
+                        size={24}
+                        color='white'
+                        onPress={ () => navigation.toggleDrawer() } />
+    })
+});
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container}
@@ -171,6 +190,19 @@ const MainNavigator = createDrawerNavigator({
             <Icon name='address-card'
                   type='font-awesome'
                   size={22}
+                  color={tintColor} />
+          )
+        },
+      },
+    Reservation:
+      { screen: ReservationNavigator,
+        navigationOptions: {
+          title: 'Reserve Table',
+          drawerLabel: 'Reserve Table',
+          drawerIcon: ({ tintColor }) => (
+            <Icon name='cutlery'
+                  type='font-awesome'
+                  size={24}
                   color={tintColor} />
           )
         },
---
+++ ios.sh
@@ -0,0 +1,2 @@
+cd confusion
+expo start --ios
