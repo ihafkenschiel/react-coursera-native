55ec7f53e88886be0c8e072d26241fda8998f2e5
Ian <ihafkenschiel@gmail.com>
2020-04-04 20:28:32 -0300

    Swipeout and deleting favorites

--- confusion/redux/ActionCreators.js
+++ confusion/redux/ActionCreators.js
@@ -154,6 +154,11 @@ export const addFavorite = (dishId) => ({
   payload: dishId
 });
 
+export const deleteFavorite = (dishId) => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: dishId
+});
+
 export const postComment = (dishId, rating, author, comment) => (dispatch) => {
 
     const newComment = {
