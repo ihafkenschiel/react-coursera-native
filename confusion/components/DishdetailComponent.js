0b6c0d599f3d61c0aeeef75680dba2c22a25963c
Ian <ihafkenschiel@gmail.com>
2020-04-05 12:49:20 -0300

    Animating gesture grant

--- confusion/components/DishdetailComponent.js
+++ confusion/components/DishdetailComponent.js
@@ -23,6 +23,8 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
   const dish = props.dish;
 
+  handleViewRef = ref => this.view = ref;
+
   const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
     if (dx < -200) // negative is further in that direction
       return true;
@@ -31,9 +33,13 @@ function RenderDish(props) {
   };
 
   const panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: (e, gestureState) => {
-            return true;
-          },
+    onStartShouldSetPanResponder: (e, gestureState) => {
+        return true;
+      },
+    onPanResponderGrant: () => {
+      this.view.rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
+    },
     onPanResponderEnd: (e, gestureState) => {
       if (recognizeDrag(gestureState))
         Alert.alert(
@@ -60,7 +66,8 @@ function RenderDish(props) {
   if (dish != null) {
     return(
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
-        {...panResponder.panHandlers}>
+                ref={this.handleViewRef}
+                {...panResponder.panHandlers}>
         <Card
             featuredTitle={dish.name}
             image={{uri: baseUrl + dish.image}}>
