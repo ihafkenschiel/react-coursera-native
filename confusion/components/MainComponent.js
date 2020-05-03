c94568698a80102f6371ca8105da2a7ce358e2d9
Ian <ihafkenschiel@gmail.com>
2020-05-03 18:14:15 -0300

    Secure store to store login variables

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -6,6 +6,7 @@ import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -141,6 +142,23 @@ const FavoritesNavigator = createStackNavigator({
     })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={ () => navigation.toggleDrawer() } />
+  })
+});
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container}
@@ -161,6 +179,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login:
+      { screen: LoginNavigator,
+        navigationOptions: {
+          title: 'Login',
+          drawerLabel: 'Login',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='sign-in'
+              type='font-awesome'
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
+      },
     Home:
       { screen: HomeNavigator,
         navigationOptions: {
@@ -240,6 +273,7 @@ const MainNavigator = createDrawerNavigator({
         },
       }
 }, {
+  initialRouteName: 'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
--- confusion/shared/baseUrl.js
+++ confusion/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.0.100:3001/'
+export const baseUrl = 'http://192.168.0.102:3001/'
